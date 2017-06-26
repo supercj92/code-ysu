0a93b444a0ff2d66b1488206774929a6eae4304a
longcangjian <longcangjian@jd.com>
2017-06-26 14:45:12 +0800

    groovy-hello-world

--- src/test/java/com/cfysu/junit/UnitTestV2.java
+++ src/test/java/com/cfysu/junit/UnitTestV2.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 
 import com.cfysu.model.Ford;
 
+import java.util.Random;
+
 public class UnitTestV2 {
+
+	private Random random = new Random();
 	@Test
 	public void testConstruct(){
 		new Ford();
@@ -13,6 +17,18 @@ public class UnitTestV2 {
 	@Test
 	public void testDivid(){
 		System.out.println(10/3);
+	}
+
+	@Test
+	public void testVerificationCode(){
+		int verficationCode = random.nextInt(9999) % (9999-1000+1) + 1000;
+		System.out.print(verficationCode);
+	}
+
+	@Test
+	public void testGetName(){
+		System.out.print(UnitTest.class.getName());
+		ClassLoader clazz = UnitTest.class.getClassLoader();
 	}
 
 }
